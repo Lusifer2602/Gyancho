09b6db3113efdd1c5aa9f668947d1006c74df4cb
bosswhopper <94573164+Lusifer2602@users.noreply.github.com>
2025-04-06 04:55:15 +0000

    enabled portforwarding

--- app.py
+++ app.py
@@ -272,7 +272,9 @@ def add_announcement():
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+# if __name__ == '__main__':
+#     if not os.path.exists(app.config['UPLOAD_FOLDER']):
+#         os.makedirs(app.config['UPLOAD_FOLDER'])
+#     app.run(debug=True)
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
